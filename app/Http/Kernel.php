c25fda809f9af66e95fa46576de001750d6efa17
devPinheiro <pinheirolaoluwa@gmail.com>
2021-01-20 23:53:01 +0100

    fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -59,6 +60,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
         'clearance' => \App\Http\Middleware\clearance::class,
-        'cors' => \App\Http\Middleware\Cors::class
+        // 'cors' => \App\Http\Middleware\Cors::class
     ];
 }
